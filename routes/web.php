cfcc8807799911737662e14e7a6de59d2edeae4f
Raed Ghanem <raed@rouf.io>
2026-01-30 14:23:55 +0200

    added the routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('tasks.index');
 });
+
+// Project routes
+Route::get('/projects/create', [ProjectController::class, 'create'])->name('projects.create');
+Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
+
+// Task routes
+Route::resource('tasks', TaskController::class)->except(['show', 'create']);
+Route::post('/tasks/reorder', [TaskController::class, 'reorder'])->name('tasks.reorder');
